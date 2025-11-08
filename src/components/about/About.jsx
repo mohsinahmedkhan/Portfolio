e732ae8c9a7402242bc96bad20c5853d73136ad6
Mohsin Ahmed Khan <mohsinahmedkhan667@gmail.com>
2025-11-07 16:19:32 -0800

    Fix: image name case issue

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Resume from '../../assets/Mohsin ahmed Khan resume.pdf'
+import Resume from '../../assets/Mohsinahmedkhanresume.pdf'
 
 const About = () => {
   useEffect(() => {
